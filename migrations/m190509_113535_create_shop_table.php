35e420ff63e0f8ab5238c214b540c213829090ca
Yarcrazy <yacrazy@yandex.ru>
2019-05-09 19:04:23 +0700

    Создал миграцию таблицы shop

---
+++ migrations/m190509_113535_create_shop_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%shop}}`.
+ */
+class m190509_113535_create_shop_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%shop}}', [
+            'shopId' => $this->primaryKey(),
+            'shopShortName' => $this->string()->notNull(),
+            'shopFullName' => $this->string()->notNull(),
+            'shopPhoto' => $this->string()->notNull(),
+            'shopType' => "enum('food', 'child', 'sport', 'beauty', 'buy')",
+            'shopPhone' => $this->string()->notNull(),
+            'shopWeb' => $this->string()->notNull(),
+            'shopAddress' => $this->string()->notNull(),
+            'shopCostMin' => $this->integer()->notNull(),
+            'shopCostMax' => $this->integer()->notNull(),
+            'shopMiddleCost' => "enum('1', '2', '3', '4', '5')",
+            'shopAgregator' => $this->string()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%shop}}');
+    }
+}
