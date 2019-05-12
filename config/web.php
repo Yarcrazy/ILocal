1259e8f654fb5a7463df0c67c763c4141b04e61c
Tohin666 <tohin666@gmail.com>
2019-05-12 11:43:47 +0300

    создал модуль api, и контроллер для работы через апи с сущностью shop

--- config/web.php
+++ config/web.php
@@ -11,10 +11,19 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'modules' => [
+        'api' => [
+            'class' => 'app\modules\api\Module',
+        ],
+    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'asdfasudfhqwbhfdashkashdflashd',
+            // Для того чтобы API мог принимать данные в формате JSON
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +52,15 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        // Включаем красивые адреса, в том числе для API.
         'urlManager' => [
             'enablePrettyUrl' => true,
+//            'enableStrictParsing' => true,
             'showScriptName' => false,
             'rules' => [
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'api/shop'],
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
---
+++ modules/api/controllers/ShopController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace app\modules\api\controllers;
+
+
+use yii\rest\ActiveController;
+
+class ShopController extends ActiveController
+{
+    public $modelClass = 'app\models\Shop';
+
+}
